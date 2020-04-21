0ec93a144a32562cb100b84c4675a6646a0eb969
vinnie <vinnie.mazza@gmail.com>
2020-04-21 09:22:23 -0400

    add full instance details as output

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,7 @@ output "private_addresses" {
 output "mgmt_public_ip_01" { value = "${google_compute_instance.vm_instance.0.network_interface.1.access_config.0.nat_ip}" }
 
 output "mgmt_public_ip_02" { value = "${var.instanceCount >= 2 ? "${google_compute_instance.vm_instance.1.network_interface.1.access_config.0.nat_ip}" : "none"}"}
+
+output "instance01Info" { value = google_compute_instance.vm_instance.0 }
+
+output "instance02Info" { value = "${var.instanceCount >= 2 ? "${ google_compute_instance.vm_instance.1 }" : "none"}"}
